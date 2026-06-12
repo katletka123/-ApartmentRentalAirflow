97848da9754ddb28cf60dc260efc03f6d33fd8af
katletka123 <nkoslova2000@gmail.com>
2026-06-12 23:02:59 +0200

    Add column price_per_metr

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import psycopg2
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import os
+import re
+
+
+
+def date_generate(row_date):
+    mounths = {
+        "stycznia": 1,
+        "lutego": 2,
+        "marca": 3,
+        "kwietnia": 4,
+        "maja": 5,
+        "czerwca": 6,
+        "lipca": 7,
+        "sierpnia": 8,
+        "września": 9,
+        "październik": 10,
+        "listopada": 11,
+        "grudnia": 12
+    }
+    if "dzisiaj" in row_date:
+        date = datetime.today().date()
+        return date
+    else:
+        date_list = re.search(r"(\d{1,2})\s+([a-ząćęłńóśźż]+)\s+(\d{4})", row_date.lower())
+        if date_list:
+            day = int(date_list.group(1))
+            mounth = mounths[date_list.group(2)]
+            year = int(date_list.group(3))
+            date = datetime(year, mounth, day).date()
+            return date
+
+
+
 
 load_dotenv()
 
@@ -15,7 +49,7 @@ conn = psycopg2.connect(
 )
 
 
-for i in range(1,4):
+for i in range(1,6):
     url = f"https://www.olx.pl/nieruchomosci/mieszkania/wynajem/warszawa/?page={i}&search%5Border%5D=created_at%3Adesc"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
@@ -25,14 +59,23 @@ for i in range(1,4):
     boxes = soup.find_all(attrs={'data-testid': 'l-card'})
 
     for box in boxes:
-        dist_and_date=box.find(attrs={'data-testid': 'location-date'}).text.strip().split(" - ")
-        date=district_date_box[1]
-        tmp_district=district_date_box[0].split(",")
+        district_and_date_box=box.find(attrs={'data-testid': 'location-date'}).text.strip().lower().split(" - ")
+
+        date= date_generate(district_and_date_box[1])
+
+        tmp_district= district_and_date_box[0].split(",")
         district=tmp_district[1]
 
         price=box.find(attrs={'data-testid':'ad-price'}).text.strip()
 
         area=box.find(attrs={'color':'text-global-secondary'}).text.strip().split(" - ")
+
+        area_tmp=area[0].split(" ")
+        area_tmp_coma=area_tmp[0].replace(',','.')
+        price_tmp=price.split(" ")
+        price_tmp_split=price_tmp[0]+price_tmp[1]
+        price_for_m2=round(int(price_tmp_split)/float(area_tmp_coma),2)
+        price_per_metr=f"{price_for_m2} zł/m2"
 
         id_num=box.get('id')
 
@@ -41,12 +84,12 @@ for i in range(1,4):
             'district':district,
             'date': date,
             'price': price,
-            'area': area[0]
+            'area': area[0],
+            'price_per_metr': price_per_metr
         }
         data_lists.append(data_list)
 
-    for data in data_lists:
-        print(data)
+
 
     create_table="""
         CREATE TABLE IF NOT EXISTS apart (
@@ -54,19 +97,21 @@ for i in range(1,4):
         district VARCHAR(100) NOT NULL,
         date VARCHAR(100),
         price VARCHAR(100),
-        area  VARCHAR(100)
+        area  VARCHAR(100),
+        price_per_metr VARCHAR(100)
     );
     """
 
     insert_table="""
-        INSERT INTO apart (id, district, date, price, area)
-        VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO apart (id, district, date, price, area, price_per_metr)
+        VALUES (%s, %s, %s, %s, %s, %s)
         ON CONFLICT (id) DO 
         UPDATE SET
             district=EXCLUDED.district,
             date=EXCLUDED.date,
             price=EXCLUDED.price,
-            area=EXCLUDED.area;
+            area=EXCLUDED.area,
+            price_per_metr=EXCLUDED.price_per_metr;
     """
 
     cursor = conn.cursor()
@@ -78,7 +123,7 @@ for i in range(1,4):
     for data in data_lists:
         cursor.execute(
             insert_table,
-            (data['id'], data['district'], data['date'], data['price'], data['area'])
+            (data['id'], data['district'], data['date'], data['price'], data['area'], data['price_per_metr'])
         )
 
 
