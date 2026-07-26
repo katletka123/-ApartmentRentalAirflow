f624c913260e08867e52eb63a481b194888889e8
Anastasiya Kazlova <katletka@MacBook-Air-Anastasiya.local>
2026-07-26 09:41:00 +0200

    rename sql files

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ boxes= get_new_boxes()
 
 raw_data_list= raw_list_generate(boxes)
 
-insert_raw_table=load_sql("raw/insert_raw_table.sql")
+insert_raw_table=load_sql("raw/insert_raw_table_query.sql")
 execute_many(insert_raw_table, raw_data_list)
-select_from_raw=load_sql("silver/select_from_raw.sql")
+select_from_raw=load_sql("silver/select_from_raw_query.sql")
 data_from_raw = execute_fethall(select_from_raw)
 
 silver_data_list= raw_transform_to_silver(data_from_raw)
-insert_silver_table=load_sql("silver/insert_silver_table.sql")
+insert_silver_table=load_sql("silver/insert_silver_table_query.sql")
 execute_many(insert_silver_table, silver_data_list)
 
 select_area_and_price_per_m2_plot_data=load_sql("gold/select_area_and_price_per_m2_plot_data.sql")
--- src/utils/database_functions.py
+++ src/utils/database_functions.py
@@ -20,9 +20,9 @@ def load_sql(relative_path: str) -> str:
     with open(sql_path, "r", encoding="utf-8") as file:
         return file.read()
 
-create_raw_table = load_sql("raw/create_raw_table.sql")
+create_raw_table = load_sql("raw/create_raw_table_query.sql")
 
-create_silver_table= load_sql("silver/create_silver_table.sql")
+create_silver_table= load_sql("silver/create_silver_table_query.sql")
 
 conn =get_connection()
 
