5f0a67d59c5fa0c7c0954c0bf56c070c5913edb8
Anastasiya Kazlova <katletka@MacBook-Air-Anastasiya.local>
2026-08-02 22:38:46 +0200

    change data type in silver and gold table varchar to numeric

--- src/source_queries/gold/create_gold_daily_market_stats.sql
+++ src/source_queries/gold/create_gold_daily_market_stats.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS gold_daily_market_stats (
     date DATE PRIMARY KEY,
     avg_price NUMERIC,
-    avg_area VARCHAR(100),
-    avg_price_per_m2 VARCHAR(100),
-    new_apartments_count VARCHAR(100)
+    avg_area NUMERIC,
+    avg_price_per_m2 NUMERIC,
+    new_apartments_count NUMERIC
 );
