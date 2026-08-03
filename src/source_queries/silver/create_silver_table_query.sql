b8b03efd4a7addfecbeeb9d24753df7bd65b0d7b
Anastasiya Kazlova <katletka@MacBook-Air-Anastasiya.local>
2026-08-03 12:32:41 +0200

    add missing comma in create_silver_tables SQL query

--- src/source_queries/silver/create_silver_table_query.sql
+++ src/source_queries/silver/create_silver_table_query.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS silver_apartments (
     id INTEGER PRIMARY KEY,
     district VARCHAR(100) NOT NULL,
-    date DATE
+    date DATE,
     price_zl NUMERIC,
     area_m2 NUMERIC,
     ready_to_negotiate BOOLEAN,
